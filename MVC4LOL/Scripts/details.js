32b262610b6655b4d67c64f1da110256ce501a80
githubaccountuser <1977Lucio@gmail.com>
2013-04-26 14:18:51 +0200

    json/details wip

--- MVC4LOL/Scripts/details.js
+++ MVC4LOL/Scripts/details.js
@@ -5,8 +5,11 @@
 
     renderStaff = function (data) {
 
-        leveling.init(data);
+        renderPatches(data);
 
+        renderChampions(data);
+
+        leveling.init(data);
 
         var date = data.Champions[0].ReleaseDate;
         var milisecs = parseInt(date.replace("/Date(", "").replace(")/", ""));
@@ -19,12 +22,43 @@
         data.Champions[0].ReleaseDate = output;
 
         $('#DetailsTemplate').tmpl(data).appendTo('#divDetails');
+    },
+    renderPatches = function (data) {
+        for (var key in data.Patches) {
+            $('#PatchTemplate').tmpl(data.Patches[key]).appendTo('#Patches');
+        }
+    },
+    renderChampions = function (data) {
+
+        $('#Champions').html("");
+
+        for (var key in data.Champions) {
+
+            var champ = data.Champions[key];
+            renderChampion(champ);
+        }
+    },
+    renderChampion = function (championData) {
+
+        var date = championData.ReleaseDate;
+        var milisecs = parseInt(date.replace("/Date(", "").replace(")/", ""));
+        var d = new Date(milisecs);
+        var month = d.getMonth() + 1;
+        var day = d.getDate();
+        var output = d.getFullYear() + '/' +
+            (('' + month).length < 2 ? '0' : '') + month + '/' +
+            (('' + day).length < 2 ? '0' : '') + day;
+        championData.ReleaseDate = output;
+
+        $('#ChampionTemplate').tmpl(championData).appendTo('#Champions');
     }
 
     return {
         init: init,
-        renderStaff: renderStaff
+        renderStaff: renderStaff,
+        renderPatches: renderPatches,
+        renderChampions: renderChampions,
+        renderChampion: renderChampion
     }
-
 
 }();
